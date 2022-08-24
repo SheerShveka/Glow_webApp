8a55cadc0c4cf3798b3c196fac3074755c44620b
pazmovich <pazmovich@gmail.com>
2022-08-24 22:47:04 +0300

    log in

--- logIn.js
+++ logIn.js
@@ -2,22 +2,19 @@ const name = document.getElementById('name')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
-
 form.addEventListener('submit', (e) => {
 	let messages = []
 	if (name.value === '' || name.value == null) {
 		messages.push('Name is required')
 	}
-
 	if (password.value.length <= 6) {
 		messages.push('Password must be longer than 6 characters')
 	}
-
 	if (password.value.length >= 20) {
 		messages.push('Password must be less than 20 characters')
 	}
 
-	if (password.value=== name) {
+	if (password.value === name) {
 		messages.push('Password cannot be the name')
 	}
 
@@ -32,6 +29,6 @@ form.addEventListener('submit', (e) => {
 		window.location.href = 'index.html'
 	}
 
-	
-	
-})
+
+
+}) 
